82aa74b1054b7d7131bf686f1408c6a06243e93e
AlejandraPG15 <alejandra.pg1602@gmail.com>
2025-03-28 19:12:31 +0100

    Cambios en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import SearchView from './components/SearchView';
+import LoginView from './components/LoginView';
+import RegisterView from './components/RegisterView';
 import './App.css';
+
+function PrivateRoute({ children }) {
+  const isAuthenticated = localStorage.getItem('isAuthenticated') === 'true';
+  return isAuthenticated ? children : <Navigate to="/login" />;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginView />} />
+        <Route path="/register" element={<RegisterView />} />
+        <Route path="/search" element={<SearchView />} />
+      </Routes>
+    </Router>
   );
 }
 
